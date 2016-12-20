01b45590d453b6d90199ddd89539fea83fc569e8
carlosSusarrey <carlos.susarrey.diaz@gmail.com>
2016-12-20 14:12:36 -0600

    add_chapter3_files
    add existing chapter3 files, there is still files to work on

---
+++ Chapter3/3.3StackOfPlates.py
@@ -0,0 +1,21 @@
+# Chapter 3 problem 3
+
+
+class SetOfStacks:
+
+    def __init__(self):
+        self.stacks = []
+        self.arbitrary_threshold = 5
+        self.stacks[0] = []
+        self.stack_count = []
+
+
+    def push(self, value):
+
+        return None
+
+    def pop(self):
+        return None
+
+    def popAt(self, index):
+        return None
